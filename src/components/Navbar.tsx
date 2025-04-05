c1001a9f080674c1a4773117f4db14ad704cf5b6
Aaushman Parth <aaushmanparth@Aaushmans-MacBook-Air.local>
2025-04-05 19:40:14 +0530

    changed logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import logo from "@/images/logo white.png";
+import logo from "@/images/logo_white.png";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
